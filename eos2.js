404a626485d29d9b3783e47c757aea200dd04eb7
duktektv <duktek.tv@gmail.com>
2024-11-29 14:17:55 +0800

    Create eos2.js

---
+++ eos2.js
@@ -0,0 +1,56 @@
+var _myJson="VzNzaWMyOXlkQ0k2TVRjek1qZzBOVFl3TUN3aWEyVjVJam9pUjNWaGRHVnRZV3hoWDB4cFoyRmZUbUZqYVc5dVlXeEJiblJwWjNWaFgwZEdRME5UUkY5TmRXNXBZMmx3WVd3aUxDSnVZVzFoWDJWMlpXNTBJam9pUjNWaGRHVnRZV3hoSUV4cFoyRWdUbUZqYVc5dVlXd2lMQ0pxWVdSM1lXeGZaWFpsYm5RaU9pSXlNREkwTFRFeExUSTVWREV3T2pBd0lpd2ljM1J2Y0Y5bGRtVnVkQ0k2SWpJd01qUXRNVEV0TWpsVU1USTZNVFVpTENKd2JHRjVaWEpmTVNJNklrRnVkR2xuZFdFZ1IwWkRJaXdpY0d4aGVXVnlYeklpT2lKRFUwUWdUWFZ1YVdOcGNHRnNJaXdpYkc5bmIxOHhJam9pYUhSMGNITTZMeTlwYldjd0xtRnBjMk52Y21VdVkyOXRMMlp2YjNSaVlXeHNMM1JsWVcwdllqTmpOalptTm1ZMU1tTXpOVFJqT0dZM01qZ3haVFE1TkRnMVlqSm1NbUl1Y0c1bklYY3pNQ0lzSW14dloyOWZNaUk2SW1oMGRIQnpPaTh2YVcxbk1DNWhhWE5qYjNKbExtTnZiUzltYjI5MFltRnNiQzkwWldGdEx6TXhORGt4TVRNek5UQTVOR05tT1RjNE4yUTFOemt4WXpnMVptUm1OamMyTG5CdVp5RjNNekFpTENKMWNteGZiVE4xT0NJNkltaDBkSEJ6T2k4dmQyRjRMbVZ0WW14aGVtOXViaTVqYjIwdmJHbDJaUzkyTVM4eVdtSlNObGR1YzFSdFVsVkVXakJDUWlzdFdTdHlWV3MyYjFKRFZ6bEdLekJSY2tveGVVMDJXa3RWTlVaU2F5MUpZUzFUYUZvMUxXMWphMHN3Y0hwT0wyMWhjM1JsY2k1dE0zVTRJbjBzZXlKemIzSjBJam94TnpNek1EQXdOREF3TENKclpYa2lPaUpIZFdGMFpXMWhiR0ZmVEdsbllWOU9ZV05wYjI1aGJFTnZZbUZ1WDBsdGNHVnlhV0ZzUXk1VExrUXVYME52YlhWdWFXTmhZMmx2Ym1Weklpd2libUZ0WVY5bGRtVnVkQ0k2SWtkMVlYUmxiV0ZzWVNCTWFXZGhJRTVoWTJsdmJtRnNJaXdpYW1Ga2QyRnNYMlYyWlc1MElqb2lNakF5TkMweE1pMHdNVlF3TlRvd01DSXNJbk4wYjNCZlpYWmxiblFpT2lJeU1ESTBMVEV5TFRBeFZEQTNPakUxSWl3aWNHeGhlV1Z5WHpFaU9pSkRiMkpoYmlCSmJYQmxjbWxoYkNJc0luQnNZWGxsY2w4eUlqb2lReTVUTGtRdUlFTnZiWFZ1YVdOaFkybHZibVZ6SWl3aWJHOW5iMTh4SWpvaWFIUjBjSE02THk5cGJXY3dMbUZwYzJOdmNtVXVZMjl0TDJadmIzUmlZV3hzTDNSbFlXMHZNMkV6TVdNM09XTXlNVFU1T0RsaU1qTXhZVGd5TVRsak1ERTJZV1E0WVdVdWNHNW5JWGN6TUNJc0lteHZaMjlmTWlJNkltaDBkSEJ6T2k4dmFXMW5NQzVoYVhOamIzSmxMbU52YlM5bWIyOTBZbUZzYkM5MFpXRnRMemd4TVRNeE1qbG1aamMyWVRreE56VTNPV1kxTldOalpqRmhaR0UzWVROakxuQnVaeUYzTXpBaUxDSjFjbXhmYlROMU9DSTZJbWgwZEhCek9pOHZkMkY0TG1WdFlteGhlbTl1Ymk1amIyMHZiR2wyWlM5Mk1TOHlXbUpTTmxkdWMxUnRVbFZFV2pCQ1Fpc3RXU3R5VldzMmIxSkRWemxHS3pCUmNrb3hlVTAyV2t0Vk5VWlNheTFKWVMxVGFGbzFMVzFqYTBzd2NIcE9MMjFoYzNSbGNpNXRNM1U0SW4wc2V5SnpiM0owSWpveE56TXpNREUwT0RBd0xDSnJaWGtpT2lKSGRXRjBaVzFoYkdGZlRHbG5ZVjlPWVdOcGIyNWhiRTFoYkdGallYUmxZMjlFWlhCdmNuUnBkbTlmV0dsdVlXSmhhblZzSWl3aWJtRnRZVjlsZG1WdWRDSTZJa2QxWVhSbGJXRnNZU0JNYVdkaElFNWhZMmx2Ym1Gc0lpd2lhbUZrZDJGc1gyVjJaVzUwSWpvaU1qQXlOQzB4TWkwd01WUXdPVG93TUNJc0luTjBiM0JmWlhabGJuUWlPaUl5TURJMExURXlMVEF4VkRFeE9qRTFJaXdpY0d4aGVXVnlYekVpT2lKTllXeGhZMkYwWldOdklpd2ljR3hoZVdWeVh6SWlPaUpFWlhCdmNuUnBkbThnV0dsdVlXSmhhblZzSWl3aWJHOW5iMTh4SWpvaWFIUjBjSE02THk5cGJXY3dMbUZwYzJOdmNtVXVZMjl0TDJadmIzUmlZV3hzTDNSbFlXMHZPR1U1TmpkbE1UQTFNRFUxTldNM01XTTBPRGhqWkRZMFpUSTFNbUZsTm1JdWNHNW5JWGN6TUNJc0lteHZaMjlmTWlJNkltaDBkSEJ6T2k4dmFXMW5NQzVoYVhOamIzSmxMbU52YlM5bWIyOTBZbUZzYkM5MFpXRnRMMlptWlRFeVpqSm1NelEyWTJOaFl6VXlPRE01TURZME9HWTNZekprWW1JeUxuQnVaeUYzTXpBaUxDSjFjbXhmYlROMU9DSTZJbWgwZEhCek9pOHZkMkY0TG1WdFlteGhlbTl1Ymk1amIyMHZiR2wyWlM5Mk1TOHlXbUpTTmxkdWMxUnRVbFZFV2pCQ1Fpc3RXU3R5VldzMmIxSkRWemxHS3pCUmNrb3hlVTAyV2t0Vk5VWlNheTFKWVMxVGFGbzFMVzFqYTBzd2NIcE9MMjFoYzNSbGNpNXRNM1U0SW4wc2V5SnpiM0owSWpveE56TXpNRGd6TWpBd0xDSnJaWGtpT2lKSGRXRjBaVzFoYkdGZlRHbG5ZVjlPWVdOcGIyNWhiRmhsYkdGcWRWOU5RMFJsY0c5eWRHbDJiMTlOYVhoamJ5SXNJbTVoYldGZlpYWmxiblFpT2lKSGRXRjBaVzFoYkdFZ1RHbG5ZU0JPWVdOcGIyNWhiQ0lzSW1waFpIZGhiRjlsZG1WdWRDSTZJakl3TWpRdE1USXRNREpVTURRNk1EQWlMQ0p6ZEc5d1gyVjJaVzUwSWpvaU1qQXlOQzB4TWkwd01sUXdOam94TlNJc0luQnNZWGxsY2w4eElqb2lXR1ZzWVdwMUlFMURJaXdpY0d4aGVXVnlYeklpT2lKRVpYQnZjblJwZG04Z1RXbDRZMjhpTENKc2IyZHZYekVpT2lKb2RIUndjem92TDJsdFp6QXVZV2x6WTI5eVpTNWpiMjB2Wm05dmRHSmhiR3d2ZEdWaGJTOWlNR1ZqT1RnMVkyUTFOMk5pWVRNM01EUTRaR1UyTVRSak5EWTJOakJsTlM1d2JtY2hkek13SWl3aWJHOW5iMTh5SWpvaWFIUjBjSE02THk5cGJXY3dMbUZwYzJOdmNtVXVZMjl0TDJadmIzUmlZV3hzTDNSbFlXMHZNbU01WldaaE4yRTJNV0l6TjJNM05HTmhPVFF4TWprNE5UQm1OR1ExWVRZdWNHNW5JWGN6TUNJc0luVnliRjl0TTNVNElqb2lhSFIwY0hNNkx5OTNZWGd1WlcxaWJHRjZiMjV1TG1OdmJTOXNhWFpsTDNZeEx6SmFZbEkyVjI1elZHMVNWVVJhTUVKQ0t5MVpLM0pWYXpadlVrTlhPVVlyTUZGeVNqRjVUVFphUzFVMVJsSnJMVWxoTFZOb1dqVXRiV05yU3pCd2VrNHZiV0Z6ZEdWeUxtMHpkVGdpZlN4N0luTnZjblFpT2pFM016TXdPRE15TURBc0ltdGxlU0k2SWtkMVlYUmxiV0ZzWVY5TWFXZGhYMDVoWTJsdmJtRnNRMU5FWDAxMWJtbGphWEJoYkVGdWRHbG5kV0ZmUjBaRElpd2libUZ0WVY5bGRtVnVkQ0k2SWtkMVlYUmxiV0ZzWVNCTWFXZGhJRTVoWTJsdmJtRnNJaXdpYW1Ga2QyRnNYMlYyWlc1MElqb2lNakF5TkMweE1pMHdNbFF3TkRvd01DSXNJbk4wYjNCZlpYWmxiblFpT2lJeU1ESTBMVEV5TFRBeVZEQTJPakUxSWl3aWNHeGhlV1Z5WHpFaU9pSkRVMFFnVFhWdWFXTnBjR0ZzSWl3aWNHeGhlV1Z5WHpJaU9pSkJiblJwWjNWaElFZEdReUlzSW14dloyOWZNU0k2SW1oMGRIQnpPaTh2YVcxbk1DNWhhWE5qYjNKbExtTnZiUzltYjI5MFltRnNiQzkwWldGdEx6TXhORGt4TVRNek5UQTVOR05tT1RjNE4yUTFOemt4WXpnMVptUm1OamMyTG5CdVp5RjNNekFpTENKc2IyZHZYeklpT2lKb2RIUndjem92TDJsdFp6QXVZV2x6WTI5eVpTNWpiMjB2Wm05dmRHSmhiR3d2ZEdWaGJTOWlNMk0yTm1ZMlpqVXlZek0xTkdNNFpqY3lPREZsTkRrME9EVmlNbVl5WWk1d2JtY2hkek13SWl3aWRYSnNYMjB6ZFRnaU9pSm9kSFJ3Y3pvdkwzZGhlQzVsYldKc1lYcHZibTR1WTI5dEwyeHBkbVV2ZGpFdk1scGlValpYYm5OVWJWSlZSRm93UWtJckxWa3JjbFZyTm05U1ExYzVSaXN3VVhKS01YbE5ObHBMVlRWR1Vtc3RTV0V0VTJoYU5TMXRZMnRMTUhCNlRpOXRZWE4wWlhJdWJUTjFPQ0o5WFE9PQ==";var _parseEvent = function(_str){
+	$('#event_list').html('');
+	var _myJson = JSON.parse(atob(atob(_str)));	
+	var _tgl 	= '';
+	for(var _x=0,_len=_myJson.length;_x<_len;_x++){
+		var _localHTML = '';
+			if(_tgl != _tanggal(_myJson[_x].jadwal_event)){
+				_localHTML += '<div class="event-date">'+_tanggal(_myJson[_x].jadwal_event)+'</div>';
+			}
+			_localHTML += '<span data-logo1="'+_myJson[_x].logo_1+'" data-logo2="'+_myJson[_x].logo_2+'" data-tim2="'+_myJson[_x].player_2+'" data-tim1="'+_myJson[_x].player_1+'" data-url="'+_myJson[_x].url_m3u8+'" class="item" data-event-start="'+_myJson[_x].jadwal_event+'"  data-event-stop="'+_myJson[_x].stop_event+'" href="" target="frame">';
+			_localHTML += '<div class="event-name">'+_myJson[_x].nama_event+'</div>';
+			_localHTML += '<div class="fixture">';
+			_localHTML += '<div class="first">'+_myJson[_x].player_1+'</div>';
+			_localHTML += '<div data-event-start="'+_myJson[_x].jadwal_event+'"  data-event-stop="'+_myJson[_x].stop_event+'" class="vs">'+_jam(_myJson[_x].jadwal_event)+'</div>';
+			_localHTML += '<div class="second">'+_myJson[_x].player_2+'</div>';
+			_localHTML += '</div>';
+			_localHTML += '</span>';
+			$('#event_list').append(_localHTML);
+			
+		_tgl = _tanggal(_myJson[_x].jadwal_event);
+	}
+	
+	var _mainUrl = 'http://localhost/video-js.html?hls=';
+	$('.item').off('click').on('click', function(){
+		var _eventTime 	= $(this).attr('data-event-start');
+		var _stopTime 	= $(this).attr('data-event-stop');
+		_tim1 = $(this).attr('data-tim1');
+		_tim2 = $(this).attr('data-tim2');
+		_logo1 = $(this).attr('data-logo1');
+		_logo2 = $(this).attr('data-logo2');
+		_currEventTime = _eventTime;
+		var _btoaUrl = btoa($(this).attr('data-url'));
+		if(_isPlay(_eventTime) > 0){		//Belum Mulai	
+			$('#timer_wrap').show();
+			$('#timer').html(_countDownPlay(_eventTime));
+			$('#frame').hide();
+			$('#frame').attr('src', '');
+			$('#tim1').html(_tim1+'<img style="padding-left:10px;width:32px;" src="'+_logo1+'" />');
+			$('#tim2').html('<img style="padding-right:10px;width:32px;" src="'+_logo2+'" />'+_tim2);
+		} if(_isPlay(_eventTime) <= 0 && _isPlay(_stopTime) > 0){ //Sedang berlangsung
+			$('#timer_wrap').hide();
+			$('#frame').show();
+			$('#frame').attr('src', _mainUrl+_btoaUrl);
+		} else { //Pertandingan berakhir
+			$('#timer_wrap').show();
+			$('#timer').html('Pertandingan telah berakhir');
+			$('#frame').hide();
+			$('#frame').attr('src', '');
+			$('#tim1').hide();
+			$('#tim2').hide();
+		}
+	});
+	
+	
+}
+_parseEvent(_myJson);
